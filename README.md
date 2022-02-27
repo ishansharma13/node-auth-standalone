7a2690c207de6bb22401d86ed9b644784ad20ac4
Ishan Sharma <ishan@yactraq.net>
2022-02-27 17:36:55 +0000

    Flow Complete

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ On a high level, in a request-response cycle in an application, after this cycle
 
 So, in order to keep track of users, whenever a first request is made, a session object gets stored in the database, and the session ID corresponding to it is sent back as cookie.
 Now the user and the server are always kept in sync based on the values stored in session and user while making that request can always refer back to the corresponding session stored in the server with the help of session ID stored in its cookie.
+
--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const session = require('express-session');
+const MongoDBSession = require('connect-mongodb-session')(session);
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const UserModel = require('./models/User');
 const app = express();
+
+const MONGO_URL = "mongodb://localhost:27017/sessions";
+mongoose.connect(MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+}).then((res)=>{
+    console.log("Connected to Database");
+});
+
+const store = MongoDBSession({
+    uri: MONGO_URL,
+    collection: 'mySessions',
+})
 
 app.use(session({
     secret: 'abcjajajb',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: store
 }));
 
 app.use(bodyParser());
@@ -15,16 +32,54 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-app.post("/register",(req,res)=>{
+const isAuth = (req,res,next) =>{
+    if(req.session.isAuth){
+        next();
+    }else{
+        res.redirect('/login');
+    }
+
+}
+app.post("/register",async (req,res)=>{
     const {name,email,password}=req.body;
-
-    res.send(200);
-
+    let user = await UserModel.findOne({email});
+    if (user){
+        return res.redirect('/register');
+    }
+    const hashedPass = await bcrypt.hash(password,12);
+    user = new UserModel({
+        username: name,
+        email,
+        password: hashedPass
+    });
+    await user.save();
+    res.redirect('/login');
 })
 
+app.post("/login", async (req,res)=>{
+    const {email,password} = req.body;
 
+    const user = await UserModel.findOne({email});
 
+    if(!user){
+        return res.redirect('/login');
+    }
 
+    const isMatch = await bcrypt.compare(password,user.password);
+
+    if(!isMatch){
+        return res.redirect('/login');
+    }
+    req.session.isAuth = true;
+    res.redirect('/dashboard');
+});
+
+app.post("/logout",(req,res)=>{
+    req.session.destroy((err)=>{
+        if (err) throw err;
+        res.redirect("/");
+    });
+});
 
 app.get('/', function(req, res){
     console.log(req.session);
@@ -37,6 +92,10 @@ app.get('/register', function(req, res){
 app.get('/login', function(req, res){
         res.sendFile(__dirname + '/views/login.html');
         });
+app.get('/dashboard', isAuth,function(req, res){
+        res.sendFile(__dirname + '/views/dashboard.html');
+});
+    
 app.listen(3003,function(){
     console.log("Server Started at PORT 3003");
 })
